b347d48dfe67ab0cbef7e21272eba3a7caa1471d
Nico Coetzee <nicc777@gmail.com>
2024-10-12 14:08:25 +0200

    * Added user input variable that takes a list of strings * Added an option to choose the final sort order of the list * Added the final list to the user data * Updated progress

--- week-3/code/functions/main.tf
+++ week-3/code/functions/main.tf
@@ -8,6 +8,17 @@ variable "currency_selection" {
   default     = "Canada-Dollar"
 }
 
+variable "user_input" {
+  type        = list(any)
+  description = "User supplied list of strings"
+  default     = ["e", "c", "a", "b"]
+}
+
+variable "user_input_sort_ascending" {
+  type    = bool
+  default = true
+}
+
 locals {
   // Note: Function name is all lowercase. No camel casing here.
   exchange_rate_record = provider::go::exchangerate({
@@ -32,8 +43,11 @@ locals {
   exchange_rate_env_string          = "  -e EXCHANGE_RATE=${local.exchange_rate_record.exchangeRate} \\"
   exchange_rate_currency_env_string = "  -e EXCHANGE_RATE_CURRENCY=${var.currency_selection} \\"
   cat_fact_env_string               = "  -e CAT_FACT=${local.cat_fact_record.fact} \\"
+  sorted_user_input                 = var.user_input_sort_ascending ? sort(var.user_input) : reverse(sort(var.user_input))
+  user_input_final_string           = join(",", local.sorted_user_input)
+  user_input_env_string             = "  -e USER_INPUT=${local.user_input_final_string} \\"
   split_user_data                   = split("\n", tostring(local.user_data))
-  parsed_user_data                  = [slice(local.split_user_data, 0, 9), local.exchange_rate_env_string, local.exchange_rate_currency_env_string, local.cat_fact_env_string, local.split_user_data[10]]
+  parsed_user_data                  = [slice(local.split_user_data, 0, 9), local.user_input_env_string, local.exchange_rate_env_string, local.exchange_rate_currency_env_string, local.cat_fact_env_string, local.split_user_data[10]]
   joined_user_data                  = join("\n", flatten(local.parsed_user_data))
 }
 
